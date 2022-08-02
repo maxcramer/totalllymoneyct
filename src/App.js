e29d966242c7a91e1a22a7f01e54b0083baa0002
maxcramer <maximiliancramer@yahoo.com>
2022-08-02 11:32:01 +0100

    logging input values to browser

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
 import { useState } from "react";
 function App() {
-  const [userDetails, setUserDetails] = useState({});
+  const [name, setName] = useState("");
+  const [bday, setBday] = useState("");
+  const [employed, setEmployed] = useState("Full Time");
+  const [income, setIncome] = useState("");
+  const [houseNum, setHouseNum] = useState("");
+  const [pc, setPC] = useState("");
 
-  const handleSubmit = (e) => {
-    setUserDetails({ value: e.target.value });
-  };
+  const handleSubmit = (e) => {};
 
-  console.log(userDetails);
+  console.log(name);
+
   return (
     <div className="App">
       <div>
@@ -15,15 +19,29 @@ function App() {
         <form onSubmit={handleSubmit}>
           <div>
             <label>Name</label>
-            <input type="text" required />
+            <input
+              type="text"
+              required
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
           </div>
           <div>
             <label>D.O.B</label>
-            <input type="date" required />
+            <input
+              type="date"
+              required
+              value={bday}
+              onChange={(e) => setBday(e.target.value)}
+            />
           </div>
           <div>
             <label>Employment Status</label>
-            <select required>
+            <select
+              required
+              value={employed}
+              onChange={(e) => setEmployed(e.target.value)}
+            >
               <option value="Full Time">Full Time</option>
               <option value="Part Time">Part Time</option>
               <option value="Student">Student</option>
@@ -32,19 +50,40 @@ function App() {
           </div>
           <div>
             <label>Income</label>
-            <input type="number" required />
+            <input
+              type="number"
+              required
+              value={income}
+              onChange={(e) => setIncome(e.target.value)}
+            />
           </div>
           <div>
             <label>House Number</label>
-            <input type="number" required />
+            <input
+              type="number"
+              required
+              value={houseNum}
+              onChange={(e) => setHouseNum(e.target.value)}
+            />
           </div>
           <div>
             <label>Postcode</label>
-            <input type="text" required />
+            <input
+              type="text"
+              required
+              value={pc}
+              onChange={(e) => setPC(e.target.value)}
+            />
           </div>
           <button type="submit">Submit</button>
         </form>
       </div>
+      <p>{name}</p>
+      <p>{bday}</p>
+      <p>{employed}</p>
+      <p>{income}</p>
+      <p>{houseNum}</p>
+      <p>{pc}</p>
     </div>
   );
 }
