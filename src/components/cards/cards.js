fb0f95ebf978e823353a2fec1908c7ac8e7f9a31
maxcramer <maximiliancramer@yahoo.com>
2022-08-02 13:52:49 +0100

    adding some styling to cards, correct cards showing on form submit

--- src/components/cards/cards.js
+++ src/components/cards/cards.js
@@ -1,11 +1,39 @@
 import React from "react";
 
+import "./cards.css";
+
 const cards = ({ newUser: { name, bday, employed, income, houseNum, pc } }) => {
   if (employed === "Student") {
     return (
       <div>
-        <div>Student Card</div>
-        <div>Anywhere Card</div>
+        <div className="card">
+          <h1>Student Card</h1>
+          <p>APR: 18.9%</p>
+          <div className="balance">
+            <p>Balance Transfer Offer Duration: </p>
+            <p className="bold"> 0 months</p>
+          </div>
+          <div className="balance">
+            <p>Purchase Offer Duration: </p> <p className="bold"> 6 months</p>
+          </div>
+          <div className="balance">
+            <p>Credit Available: </p> <p className="bold"> £1200</p>
+          </div>
+        </div>
+        <div className="card">
+          <h1>Anywhere Card</h1>
+          <p>APR: 18.9%</p>
+          <div className="balance">
+            <p>Balance Transfer Offer Duration: </p>
+            <p className="bold"> 0 months</p>
+          </div>
+          <div className="balance">
+            <p>Purchase Offer Duration: </p> <p className="bold"> 6 months</p>
+          </div>
+          <div className="balance">
+            <p>Credit Available: </p> <p className="bold"> £1200</p>
+          </div>
+        </div>
       </div>
     );
   }
@@ -13,14 +41,40 @@ const cards = ({ newUser: { name, bday, employed, income, houseNum, pc } }) => {
     return (
       <div>
         <div>Liquid Card</div>
-        <div>Anywhere Card</div>
+        <div className="card">
+          <h1>Anywhere Card</h1>
+          <p>APR: 18.9%</p>
+          <div className="balance">
+            <p>Balance Transfer Offer Duration: </p>
+            <p className="bold"> 0 months</p>
+          </div>
+          <div className="balance">
+            <p>Purchase Offer Duration: </p> <p className="bold"> 6 months</p>
+          </div>
+          <div className="balance">
+            <p>Credit Available: </p> <p className="bold"> £1200</p>
+          </div>
+        </div>
       </div>
     );
   }
   if (employed !== "Student" && income < 16000)
     return (
       <div>
-        <div>Anywhere Card</div>
+        <div className="card">
+          <h1>Anywhere Card</h1>
+          <p>APR: 18.9%</p>
+          <div className="balance">
+            <p>Balance Transfer Offer Duration: </p>
+            <p className="bold"> 0 months</p>
+          </div>
+          <div className="balance">
+            <p>Purchase Offer Duration: </p> <p className="bold"> 6 months</p>
+          </div>
+          <div className="balance">
+            <p>Credit Available: </p> <p className="bold"> £1200</p>
+          </div>
+        </div>
       </div>
     );
 
