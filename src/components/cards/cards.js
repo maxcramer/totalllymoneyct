789d0a6282e750577564bb4272fa435fb6bd2ecf
maxcramer <maximiliancramer@yahoo.com>
2022-08-02 14:38:43 +0100

    adding another case study to the cards conditional rendering

--- src/components/cards/cards.js
+++ src/components/cards/cards.js
@@ -3,6 +3,55 @@ import React from "react";
 import "./cards.css";
 
 const cards = ({ newUser: { name, bday, employed, income, houseNum, pc } }) => {
+  if (employed === "Student" && income > 16000) {
+    return (
+      <div>
+        <div className="card">
+          <h1>Liquid Card</h1>
+          <p>APR: 33.9%</p>
+          <div className="balance">
+            <p>Balance Transfer Offer Duration: </p>
+            <p className="bold"> 12 months</p>
+          </div>
+          <div className="balance">
+            <p>Purchase Offer Duration: </p> <p className="bold"> 6 months</p>
+          </div>
+          <div className="balance">
+            <p>Credit Available: </p> <p className="bold"> £3000</p>
+          </div>
+        </div>
+        <div className="card">
+          <h1>Anywhere Card</h1>
+          <p>APR: 33.9%</p>
+          <div className="balance">
+            <p>Balance Transfer Offer Duration: </p>
+            <p className="bold"> 0 months</p>
+          </div>
+          <div className="balance">
+            <p>Purchase Offer Duration: </p> <p className="bold"> 0 months</p>
+          </div>
+          <div className="balance">
+            <p>Credit Available: </p> <p className="bold"> £300</p>
+          </div>
+        </div>
+        <div className="card">
+          <h1>Student Card</h1>
+          <p>APR: 18.9%</p>
+          <div className="balance">
+            <p>Balance Transfer Offer Duration: </p>
+            <p className="bold"> 0 months</p>
+          </div>
+          <div className="balance">
+            <p>Purchase Offer Duration: </p> <p className="bold"> 6 months</p>
+          </div>
+          <div className="balance">
+            <p>Credit Available: </p> <p className="bold"> £1200</p>
+          </div>
+        </div>
+        <h3>Total Credit: £4500</h3>
+      </div>
+    );
+  }
   if (employed === "Student") {
     return (
       <div>
@@ -90,7 +139,6 @@ const cards = ({ newUser: { name, bday, employed, income, houseNum, pc } }) => {
             <p>Credit Available: </p> <p className="bold"> £300</p>
           </div>
         </div>
-        <h3>Total Credit: £300</h3>
       </div>
     );
 };
