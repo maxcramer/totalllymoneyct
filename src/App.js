1bcfd41c050b0c2e4dfdf57fa1b33b7819f29f4f
maxcramer <maximiliancramer@yahoo.com>
2022-08-02 11:10:04 +0100

    setting up form basic html

--- src/App.js
+++ src/App.js
@@ -5,6 +5,33 @@ function App() {
     <div className="App">
       <body>
         <h1>Totally Money Code Test</h1>
+        <form action="">
+          <div>
+            <label>Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label>D.O.B</label>
+            <input type="date" />
+          </div>
+          <div>
+            <label>Employment Status</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label>Income</label>
+            <input type="number" />
+          </div>
+          <div>
+            <label>House Number</label>
+            <input type="number" />
+          </div>
+          <div>
+            <label>Postcode</label>
+            <input type="text" />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
       </body>
     </div>
   );
